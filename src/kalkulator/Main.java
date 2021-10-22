13a63894ec122bf167944635946db69d6e3e84fa
Stefan Tosic <tosic.ra29.2019@uns.ac.rs>
2021-10-22 15:34:13 +0200

    initial read loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,18 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		String s;
+		do {
+			s = scanner.nextLine();
+		} while(!s.equals("kraj"));
+		
 	}
 
 }
